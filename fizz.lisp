77fec839cf95c53caad44f6473f5abdf758a1a1b
Yoteichi <plonk@piano.email.ne.jp>
2014-04-11 11:58:28 +0900

    わからん

--- fizz.lisp
+++ fizz.lisp
@@ -1,13 +1,27 @@
+(def = (a b)
+  (.== a b))
+(def + (&rest args)
+  (.inject args 0 '+))
+(def * (&rest args)
+  (.inject args 1 '*))
+(def - (a &rest args)
+  (.inject args a '-))
+;; (def / (a &rest args)
+;;   (.inject args a '/))
+
+(def % (a b)
+  (.% a b))
+
 (def fizz (i)
-  (if (== i 20)
+  (if (= i 20)
       nil
       (progn
-        (setq p (.list (== (% i 3) 0) (== (% i 5) 0)))
+        (setq p [(= (% i 3) 0) (= (% i 5) 0)])
         (cond
-          ((== p (.list true true)) (.puts "fizzbuzz"))
-          ((== p (.list true false)) (.puts "fizz"))
-          ((== p (.list false true)) (.puts "buzz"))
-          ((== p (.list false false)) (.puts i)))
-        (.fizz (+ i 1)))))
+          ((= p [true true]) (puts "fizzbuzz"))
+          ((= p [true false]) (puts "fizz"))
+          ((= p [false true]) (puts "buzz"))
+          ((= p [false false]) (puts i)))
+        (fizz (+ i 1)))))
 
-(.fizz 1)
+(fizz 1)
